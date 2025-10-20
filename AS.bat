4c34f604453fbdc47d5104cbda5451b2c8ccc36e
Jose <jj9917384@gmail.com>
2025-10-20 15:01:06 -0400

    Running into some weird tracking issues, plz do not change :D

--- AS.bat
+++
@@ -1,2 +0,0 @@
-Assembler\asw.exe "file name".a48
-Assembler\p2bin.exe "file name".p "file name".bin -r 1024-3071
---
+++ template.bat
@@ -0,0 +1,3 @@
+Assembler\asw.exe "file name".a48
+Assembler\p2bin.exe "file name".p "file name".bin -r 1024-3071
+pause
