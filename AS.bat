a35599595d49360dfda421beaa11884a62bfbb5c
Jose <jj9917384@gmail.com>
2025-10-20 14:42:19 -0400

    Adding a hello world script using 8048 assembly, g7000 header, and Assembler

--- AS.bat
+++ AS.bat
@@ -1,3 +1,3 @@
-Assembler\asw.exe "file name".a48
-Assembler\p2bin.exe "file name".p "file name".bin -r 1024-3071
+Assembler\asw.exe moveasprite.a48
+Assembler\p2bin.exe moveasprite.p moveasprite.bin -r 1024-3071
 pause
